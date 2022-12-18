4221d0f482d9f66dfdd2e9410cfd839623068139
Jorge Epuñan <juanbrujo@gmail.com>
2022-12-18 11:50:46 -0300

    chore: modify Andtv vars enabled

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,32 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components({
-      resolvers: [AntDesignVueResolver()],
-    })],
+      resolvers: [AntDesignVueResolver({ importStyle: "less" })],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          "primary-color": "#1DA57A",
+          "link-color": "#1DA57A",
+          "border-radius-base": "16px",
+        },
+        javascriptEnabled: true,
+      },
     },
   },
 });
